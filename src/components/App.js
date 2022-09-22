7f923e09ff1ab77ce6ea244faf300493cadd2ab9
Blossom <saphashb@gmail.com>
2022-09-22 15:48:01 +0100

    create components in react application

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../../src/index.css';
+import React from 'react';
+
+const App = () => {
+  return <div className='header'>
+    <div className='header-inner'>
+      <div>
+        <h1>This is Breact</h1>
+        <p>Blossom's custom react app</p>
+      </div>
+    </div>
+  </div>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
+import App from './components/App';
 
-const App = () => {
-  return <div className='header'>
-    <div className='header-inner'>
-      <div>
-        <h1>This is Breact</h1>
-        <p>Blossom's custom react app</p>
-      </div>
-    </div>
-  </div>
+const Index = () => {
+  return <App />
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<Index />, document.getElementById('app'))
